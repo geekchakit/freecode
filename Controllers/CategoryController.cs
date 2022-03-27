fd9fedd3ab6e20d37fb009adcc7419c7a451f75d
manas <manas@LAPTOP-QSC6BNFB>
2022-03-28 01:40:06 +0530

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using freecode.Data;
+using freecode.Models;
+using Microsoft.AspNetCore.Mvc;
+
+    namespace freecode.Controllers;
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _db.Categorys;
+            return View(objCategoryList);
+        }
+    }
+
+
---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using freecode.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace freecode.Data;
+
+    public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options ): base(options)
+    {
+    }
+
+     public DbSet<Category> Categorys{ get; set; }
+}
+

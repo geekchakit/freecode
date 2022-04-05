8433c1172df1da79202aa250f311d11d0f4ab510
chakitsharma444@outlook.com <chakitsharma444@outlook.com>
2022-04-05 21:35:24 +0530

    ok

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -16,6 +16,77 @@ using Microsoft.AspNetCore.Mvc;
             IEnumerable<Category> objCategoryList = _db.Categorys;
             return View(objCategoryList);
         }
+        public IActionResult Create()
+        {
+        
+        return View();
+        }
+      [HttpPost]
+      [ValidateAntiForgeryToken]
+       public IActionResult Create(Category obj)
+       {
+        if (ModelState.IsValid)
+        {
+            _db.Categorys.Add(obj);
+            _db.SaveChanges();
+            TempData["successs"] = "Category created succesfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+       }
+    public IActionResult Edit(int? id)
+    {
+        if(id == null|| id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categorys.Find(id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+            return View(categoryFromDb);
     }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if (ModelState.IsValid)
+        {
+            _db.Categorys.Update(obj);
+            _db.SaveChanges();
+            TempData["successs"] = "Category Edited succesfully";
+            return View();
+        }
+        return View(obj);
+    }
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categorys.Find(id);
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var obj = _db.Categorys.Find(id);
+        if (obj == null)
+        {
+            return NotFound();
+        }
+        _db.Categorys.Remove(obj);
+            _db.SaveChanges();
+        TempData["successs"] = "Category Deleted succesfully";
+        return RedirectToAction("Index");
+    }
+}
 
 
--- Models/Category.cs
+++ Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace freecode.Models;
 
@@ -7,7 +8,8 @@ namespace freecode.Models;
         public int Id { get; set; }
         [Required]
         public string? Name { get; set; }
-
+        [DisplayName("Display Order")]
+        [Range(1,100,ErrorMessage ="Display order should between 100")]
         public int DisplayOrder { get; set; }
 
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
